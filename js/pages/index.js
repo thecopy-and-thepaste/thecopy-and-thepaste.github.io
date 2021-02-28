096af238d8fd4d0348f944fa58b517279e6735d7
thecopy-and-thepaste <thecopy.and.thepaste@gmail.com>
2021-02-28 16:07:50 -0600

    Landing page/about and dependencies

---
+++ js/pages/index.js
@@ -0,0 +1,23 @@
+var re_space = new RegExp(" ", 'g')
+
+
+class Home {
+
+}
+
+(function ($) {
+    var loading = false
+
+   
+
+    // Hiding overlay
+    $('#overlay').hide()
+
+
+
+   
+
+
+
+})(jQuery)
+
